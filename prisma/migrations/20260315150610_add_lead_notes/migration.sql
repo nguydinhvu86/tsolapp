001f56443c67ba82264cdb336ed22bd74bb47420
Admin <admin@example.com>
2026-03-15 22:06:38 +0700

    Add missing database migration for LeadNote

---
+++ prisma/migrations/20260315150610_add_lead_notes/migration.sql
@@ -0,0 +1,42 @@
+-- AlterTable
+ALTER TABLE `leadcomment` ADD COLUMN `parentId` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `LeadNote` (
+    `id` VARCHAR(191) NOT NULL,
+    `leadId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+    `attachment` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LeadCommentReaction` (
+    `id` VARCHAR(191) NOT NULL,
+    `emoji` VARCHAR(191) NOT NULL,
+    `commentId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `LeadCommentReaction_commentId_userId_emoji_key`(`commentId`, `userId`, `emoji`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `LeadNote` ADD CONSTRAINT `LeadNote_leadId_fkey` FOREIGN KEY (`leadId`) REFERENCES `Lead`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LeadNote` ADD CONSTRAINT `LeadNote_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LeadComment` ADD CONSTRAINT `LeadComment_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `LeadComment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LeadCommentReaction` ADD CONSTRAINT `LeadCommentReaction_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `LeadComment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `LeadCommentReaction` ADD CONSTRAINT `LeadCommentReaction_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
