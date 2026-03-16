0c71c6f3768abe0253cebb68e4a3bfa28dba028d
Admin <admin@example.com>
2026-03-16 13:33:14 +0700

    fix: dynamically load vapid public key to fix stale nextjs static cache

---
+++ run-remote-build.js
@@ -0,0 +1,29 @@
+const { Client } = require('ssh2');
+
+const conn = new Client();
+const password = 'P@ssw0rdVu';
+
+conn.on('ready', () => {
+    console.log('Connected to server. Executing remote build...');
+
+    const cmd = `cd /www/wwwroot/inside.tsol.vn/tsolapp && ` +
+        `export PATH=/www/server/nvm/versions/node/v24.14.0/bin:$PATH && ` +
+        `npm run build`;
+
+    conn.exec(cmd, (err, stream) => {
+        if (err) throw err;
+
+        stream.on('close', (code, signal) => {
+            conn.end();
+        }).on('data', (data) => {
+            process.stdout.write(data.toString());
+        }).stderr.on('data', (data) => {
+            process.stderr.write(data.toString());
+        });
+    });
+}).connect({
+    host: '124.158.9.5',
+    port: 22,
+    username: 'incall',
+    password: password
+});
